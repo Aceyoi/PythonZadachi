6e267bb7a36d6bfdc1a6188cfdc7f423cc1902cf
Aceyoi <146310875+Aceyoi@users.noreply.github.com>
2025-02-16 12:27:06 +0900

    Add files via upload

--- Zadachi678modul.py
+++ Zadachi678modul.py
@@ -1,14 +1,11 @@
 from array import *
 import random
+import numpy as np
 
 def testcheck():
     """Функция проверки"""
 
     pass
-
-def randomi(n):
-    """Функция создания и заполнения матрицы случайными числами"""
-    return [[random.randint(1, 100) for _ in range(n)] for _ in range(n)]
 
 def image(matrix):
     """Функция вывода матрицы"""
@@ -18,8 +15,8 @@ def image(matrix):
 
 def transform(matrix, n):
     """Преобразование матрицы: строка n становится столбцом n, и наоборот."""
-    transformed = [row[:] for row in matrix]  # Создаем копию матрицы
-    for i in range(len(matrix)):
-        transformed[i][n] = matrix[n][i]
-        transformed[n][i] = matrix[i][n]
+    transformed = matrix.copy()  # Создаем копию матрицы
+    for i in range(matrix.shape[0]):
+        transformed[i, n] = matrix[n, i]
+        transformed[n, i] = matrix[i, n]
     return transformed
